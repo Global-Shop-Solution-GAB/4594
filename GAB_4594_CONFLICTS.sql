27142c966db344af98b2d513c2d7e643e4667423
Service Web <sw@gssmail.com>
2024-03-12 11:11:33 -0500

    Commit changes in C:\Websites\Git\emarlow\4594\GAB_4594_CONFLICTS.sql

---
+++ GAB_4594_CONFLICTS.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GAB_4594_CONFLICTS
+ * ========================================== */
+CREATE TABLE "GAB_4594_CONFLICTS"(
+ "SCODE1" CHAR(6) NOT NULL ,
+ "SCODE2" CHAR(6) NOT NULL ,
+ "column0" CHAR(6) NOT NULL ,
+ "column1" CHAR(6) NOT NULL );
+
+
