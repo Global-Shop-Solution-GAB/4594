a7bc4e4b32a0baa5de407e041682ab84fcff381c
Service Web <sw@gssmail.com>
2024-03-12 11:11:33 -0500

    Commit changes in C:\Websites\Git\emarlow\4594\GAB_4594_BATCH.sql

---
+++ GAB_4594_BATCH.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GAB_4594_BATCH
+ * ========================================== */
+CREATE TABLE "GAB_4594_BATCH"(
+ "BATCHED" BIT DEFAULT '0' NOT NULL ,
+ "JOB" CHAR(6) NOT NULL ,
+ "SUFFIX" CHAR(3),
+ "SEQ" CHAR(6) NOT NULL );
+
+
