9989f7c83cd31afe0a10189e2616c72f632a26b2
Service Web <sw@gssmail.com>
2024-03-12 11:11:33 -0500

    Commit changes in C:\Websites\Git\emarlow\4594\GAB_4594_HIGHLIGHT.sql

---
+++ GAB_4594_HIGHLIGHT.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GAB_4594_HIGHLIGHT
+ * ========================================== */
+CREATE TABLE "GAB_4594_HIGHLIGHT"(
+ "SCODE" CHAR(6) NOT NULL ,
+ "DESCRIPTION" CHAR(30) NOT NULL ,
+ "column0" CHAR(6) NOT NULL ,
+ "column1" CHAR(30) NOT NULL );
+
+
